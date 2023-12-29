018e8ef28de22766c87aa5c3b92ba3b5aac6a2bf
WongYC19 <ycfkjc@Hotmail.com>
2023-12-29 23:52:52 +0800

    Create simple navbar with theme switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,19 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        colorScheme="green"
+        size="md"
+      />
+      <Text>Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
